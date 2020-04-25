26ff53685545e7ac2661d613043412d21082925c
Arsene Kevin <kouomeukevin@gmail.com>
2020-04-25 15:17:50 +0200

    finish exercice 4

--- dom-assignment-start/assignment.js
+++ dom-assignment-start/assignment.js
@@ -1,2 +1,13 @@
 const getTask1 = document.querySelector('li:first-child');
-const getTask2 = document.getElementById('#task-1')
+const getTask2 = document.getElementById('task-1');
+
+const getTitle1 = document.querySelector('title');
+const getTitle2 = document.querySelector('head > title');
+
+const getHeader = document.querySelector('h1');
+
+getTask1.style.backgroundColor = 'black';
+getTask2.style.color = 'white';
+
+getTitle1.innerText = 'Assignment - Solved!';
+getHeader.innerText = 'Assignment - Solved!'

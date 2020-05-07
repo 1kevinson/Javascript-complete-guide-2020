3999d79e6535d6a8d5f24fdbcf6a8d164a9ab338
Arsene Kevin <kevinkouomeu@icloud.com>
2020-05-07 23:43:01 +0200

    bind item, with parameter

--- practice-oop-01-starting-setup/assets/scripts/app.js
+++ practice-oop-01-starting-setup/assets/scripts/app.js
@@ -1,10 +1,21 @@
 /** @format */
 
+class DOMHelper {
+  static moveElement(elementId, newDestinationSelector) {
+    const element = document.getElementById(elementId);
+    const destinationElement = document.querySelector(newDestinationSelector);
+    destinationElement.append(element);
+  }
+}
+
 class Tooltip {}
 
 class ProjectItem {
-  constructor(id) {
+  constructor(id, updateProjectListsFunction) {
     this.id = id;
+    this.updateProjectListsHandler = updateProjectListsFunction;
+    this.connectMoreInfoButton();
+    this.connectSwitchButton();
   }
 
   connectMoreInfoButton() {}
@@ -12,23 +23,39 @@ class ProjectItem {
   connectSwitchButton() {
     const projectItemElement = document.getElementById(this.id);
     const switchBtn = projectItemElement.querySelector('button:last-of-type');
-    switchBtn.addEventListener('click',);
+    // Bind event click with this project Item and set parameter to bind
+    switchBtn.addEventListener('click', this.updateProjectListsHandler.bind(this, this.id));
   }
 }
 
 class ProjecList {
   projects = [];
+  Helper = new DOMHelper();
 
   constructor(type) {
+    this.type = type;
     const prjItems = document.querySelectorAll(`#${type}-projects li`);
     for (const prjItem of prjItems) {
-      this.projects.push(new ProjectItem(prjItem.id));
+      this.projects.push(
+        // Bind project Item with the parameter Id define in -> updateProjectListsHandler <-
+        new ProjectItem(prjItem.id, this.switchProject.bind(this))
+      );
     }
+    console.log(this.projects);
   }
 
-  addProject() {}
+  setSwitchHandlerFunction(switchHandlerFunction) {
+    this.switchHandler = switchHandlerFunction;
+  }
+
+  addProject(project) {
+    this.projects.push(project);
+    DOMHelper.moveElement(project.id, `#${type}-projects ul`);
+    console.log(this);
+  }
 
   switchProject(projectId) {
+    this.switchHandler(this.projects.find((p) => p.id === projectId));
     //Filter array to remove unmatching project p
     this.projects = this.projects.filter((p) => p.id !== projectId);
   }
@@ -38,6 +65,12 @@ class App {
   static init() {
     const activesProjectList = new ProjecList('active');
     const finishedProjectList = new ProjecList('finished');
+    activesProjectList.setSwitchHandlerFunction(
+      finishedProjectList.addProject.bind(finishedProjectList)
+    );
+    finishedProjectList.setSwitchHandlerFunction(
+      activesProjectList.addProject.bind(activesProjectList)
+    );
   }
 }
 

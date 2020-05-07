22b29f578b8fabe9227fe3380532ff06b3afd4f5
Arsene Kevin <kouomeukevin@gmail.com>
2020-05-07 21:44:29 +0200

    section 12 - ch 281

---
+++ practice-oop-01-starting-setup/assets/scripts/app.js
@@ -0,0 +1,29 @@
+/** @format */
+
+class Tooltip {}
+
+class ProjectItem {
+  constructor(id) {
+    this.id = id;
+  }
+}
+
+class ProjecList {
+  projects = [];
+
+  constructor(type) {
+    const prjItems = document.querySelectorAll(`#${type}-projects li`);
+    for (const prjItem of prjItems) {
+      this.projects.push(new ProjectItem(prjItem.id));
+    }
+  }
+}
+
+class App {
+  static init() {
+    const activesProjectList = new ProjecList('active');
+    const finishedProjectList = new ProjecList('finished');
+  }
+}
+
+App.init();

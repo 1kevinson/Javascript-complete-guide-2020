ce4dd308450f932d42d5b3625f9d564f80be61be
Arsene Kevin <kevinkouomeu@icloud.com>
2020-05-15 09:47:09 +0200

    Factory functions

--- adv-fn-01-starting-setup/app.js
+++ adv-fn-01-starting-setup/app.js
@@ -1,10 +1,10 @@
-//Pure function will always return the same result
+//Pure function will always return the same result (more pure functions)
 function sum(num1, num2) {
   return num1 + num2;
 }
 console.log(sum(2, 6));
 
-// Impure function will always return a different result
+// Impure function will always return a different result (less impure functions)
 function addRandom(num1) {
   return num1 + Math.random();
 }
@@ -18,3 +18,17 @@ function addMoreNumbers(num1, num2) {
   return sum;
 }
 console.log(addMoreNumbers(2, 3));
+
+//Factory functions (use a function inside a function)
+function createTaxCalculator(tax) {
+  function calculateTax(amount) {
+    return amount * tax;
+  }
+  return calculateTax;
+}
+
+const cVatAmount = createTaxCalculator(0.19);
+const cIcomeTaxAmount = createTaxCalculator(0.25);
+
+console.log(cVatAmount(200));
+console.log(cIcomeTaxAmount(300));

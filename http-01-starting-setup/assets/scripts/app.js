55e3fa109f30729b465467ca9f24db2a168a487b
Arsene Kevin <kevinkouomeu@icloud.com>
2020-05-19 00:37:24 +0200

    Adding Request Header

--- http-01-starting-setup/assets/scripts/app.js
+++ http-01-starting-setup/assets/scripts/app.js
@@ -38,6 +38,9 @@ function sendHttpRequestWithFetch(method, url, data) {
   return fetch(url, {
     method: method,
     body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
   }).then((response) => {
     return response.json();
   });

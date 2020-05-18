575111443721692f62a558060674c586c0af3c75
Arsene Kevin <kevinkouomeu@icloud.com>
2020-05-18 23:07:17 +0200

    Promisifying Http Requests

--- http-01-starting-setup/assets/scripts/app.js
+++ http-01-starting-setup/assets/scripts/app.js
@@ -1,22 +1,45 @@
 const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 
-const xhr = new XMLHttpRequest();
+function sendHttpRequest(method, url) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
-xhr.responseType = "json";
+    xhr.open(method, url);
+    xhr.responseType = "json";
 
-// Asynchronous function , JS will not block code even if the datas has not been retrieve yet
-xhr.onload = function () {
-  const listOfPosts = xhr.response;
+    // Asynchronous function , JS will not block code even if the data has not been retrieve yet
+    xhr.onload = function () {
+      resolve(xhr.response);
+    };
 
-  for (const post of listOfPosts) {
+    xhr.send();
+  });
+
+  return promise;
+}
+
+async function fetchPosts() {
+  const responseData = await sendHttpRequest(
+    "GET",
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+
+  for (const post of responseData) {
     // Use template to set new node in DOM
     const postEl = document.importNode(postTemplate.content, true);
     postEl.querySelector("h2").textContent = post.title;
     postEl.querySelector("p").textContent = post.body;
     listElement.append(postEl);
   }
-};
+}
 
-xhr.send();
+fetchPosts();
+
+/*
+ * TYPO
+ *
+ * ---- Use async for a function which call a promise inside him, then we are able to retrieve promise data with await  ----
+ *
+ *
+ * */

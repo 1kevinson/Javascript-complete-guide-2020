19188d9014b4fec5361a242bdd492c27f8f606a6
Arsene Kevin <kevinkouomeu@icloud.com>
2020-05-18 22:30:52 +0200

    Json Data & Parsing Data

--- http-01-starting-setup/assets/scripts/app.js
+++ http-01-starting-setup/assets/scripts/app.js
@@ -1,4 +1,21 @@
+const listElement = document.querySelector(".posts");
+const postTemplate = document.getElementById("single-post");
+
 const xhr = new XMLHttpRequest();
 
 xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+xhr.responseType = "json";
+
+// Asynchronous function , JS will not block code even if the datas has not been retrieve yet
+xhr.onload = function () {
+  const listOfPosts = xhr.response;
+
+  for (const post of listOfPosts) {
+    const postEl = document.importNode(postTemplate.content, true);
+    postEl.querySelector("h2").textContent = post.title;
+    postEl.querySelector("p").textContent = post.body;
+    listElement.append(postEl);
+  }
+};
+
 xhr.send();

c682c49f501080e89ad80b066af9853b82661748
Arsene Kevin <kevinkouomeu@icloud.com>
2020-05-18 23:27:28 +0200

    Sending Data with post request

--- http-01-starting-setup/assets/scripts/app.js
+++ http-01-starting-setup/assets/scripts/app.js
@@ -1,7 +1,7 @@
 const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 
-function sendHttpRequest(method, url) {
+function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
 
@@ -13,7 +13,7 @@ function sendHttpRequest(method, url) {
       resolve(xhr.response);
     };
 
-    xhr.send();
+    xhr.send(JSON.stringify(data));
   });
 
   return promise;
@@ -34,7 +34,23 @@ async function fetchPosts() {
   }
 }
 
+async function createPost(title, content) {
+  const userId = Math.random();
+  const post = {
+    title: title,
+    body: content,
+    userId: userId,
+  };
+
+  await sendHttpRequest(
+    "POST",
+    "https://jsonplaceholder.typicode.com/posts",
+    post
+  );
+}
+
 fetchPosts();
+createPost("YO kev", "Dummy post by Kev");
 
 /*
  * TYPO

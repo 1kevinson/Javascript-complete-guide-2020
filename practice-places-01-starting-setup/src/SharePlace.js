ab44f411561de96d8ac176db4e4a2b0a339d5719
Arsene Kevin <kevinkouomeu@icloud.com>
2020-05-22 02:42:00 +0200

    getting the user location ( and installing swal -> sweet alert )

--- practice-places-01-starting-setup/src/SharePlace.js
+++ practice-places-01-starting-setup/src/SharePlace.js
@@ -1,13 +1,34 @@
+import swal from "sweetalert";
+
 class PlaceFinder {
   constructor() {
     const addressForm = document.querySelector("form");
     const locateUserBtn = document.getElementById("locate-btn");
 
     locateUserBtn.addEventListener("click", this.locateUserHandler);
-    addressForm.addEventListener("submit", this.findAdressHandler);
+    addressForm.addEventListener("submit", this.findAddressHandler);
   }
 
-  locateUserHandler() {}
+  locateUserHandler() {
+    if (!navigator.geolocation) {
+      swal("Location is not available", "please allow geolocation", "error");
+      return;
+    }
+    navigator.geolocation.getCurrentPosition(
+      (success) => {
+        const coordinates = {
+          lat: success.coords.latitude + Math.random() * 50,
+          lng: success.coords.longitude + Math.random() * 50,
+        };
+        console.log(coordinates);
+      },
+      (error) => {
+        swal("Couldn't locate you unfortunately...", error.message);
+      }
+    );
+  }
 
-  findAdressHandler() {}
+  findAddressHandler() {}
 }
+
+new PlaceFinder();

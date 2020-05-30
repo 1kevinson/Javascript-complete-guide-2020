f160cb369b879010d7354044d74b73ef262a088b
Arsene Kevin <kevinkouomeu@icloud.com>
2020-05-30 16:30:21 +0200

    Writing & Running E2E Tests

--- testing-01-starting-setup/util.test.js
+++ testing-01-starting-setup/util.test.js
@@ -1,6 +1,6 @@
 // Import function in a constant supported by Jest
-const { checkAndGenerate } = require("./util");
-const { generateText } = require("./util");
+const puppeteer = require("puppeteer");
+const { generateText, checkAndGenerate } = require("./util");
 
 test("Should output name and age", () => {
   const text = generateText("Kev", 27);
@@ -10,4 +10,19 @@ test("Should output name and age", () => {
 test("Should generate a valid text output", () => {
   const text = checkAndGenerate("Kev", 27);
   expect(text).toBe("Kev (27 years old)");
+});
+
+test("should click around", async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    slow: 80,
+    args: ["--window-size=1820,1080"],
+  });
+  const page = await browser.newPage();
+  await page.goto("http://localhost:5000/testing-01-starting-setup/");
+  await page.click("input#name");
+  await page.type("input#name", "Anna");
+  await page.click("input#age");
+  await page.type("input#age", "25");
+  await page.click("button#btnAddUser");
 });

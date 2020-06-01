8ded9a5611810e3c40c25fad690405418a4dfd10
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-01 12:54:27 +0200

    Algorythms - sorted array of numbers

--- algo-01-starting-setup/alg-1-min.js
+++ algo-01-starting-setup/alg-1-min.js
@@ -1,4 +1,4 @@
-// Algoryhtm find the minimum value
+// Algorithm find the minimum value first method
 function getMin(numbers) {
   if (numbers.length === 0) {
     throw new Error("Should not be an empty array!");
@@ -15,8 +15,36 @@ function getMin(numbers) {
   return currentMinimum;
 }
 
-const testArray = [31, 9, 55, 4, 62, 8];
+// Algorithm find the minimum value second method : sorted
+function getMin2(numbers) {
+  if (numbers.length === 0) {
+    throw new Error("Should not be an empty array!");
+  }
 
-const min = getMin(testArray);
+  for (let i = 0; i < numbers.length; i++) {
+    let prevNumber = numbers[i];
+    for (let j = i + 1; j < numbers.length; j++) {
+      let nextNumber = numbers[j];
+
+      if (prevNumber > nextNumber) {
+        //Swap (replacement position)
+        numbers[i] = nextNumber;
+        numbers[j] = prevNumber;
+
+        console.log(numbers);
+
+        //Updates the positions
+        prevNumber = numbers[i];
+        nextNumber = numbers[j];
+      }
+    }
+  }
+
+  return numbers[0];
+}
+
+const testArray = [31, 9, 55, 4, 62, -6, 8];
+
+const min = getMin2(testArray);
 
 console.log(min); // should be 4

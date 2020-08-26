fa6403a8f9abe97c37f37218206c8247f6f59433
Arsene Kevin <kevinkouomeu@icloud.com>
2020-08-26 03:16:42 +0200

    add characters

--- arrays-02-creating-arrays/array.js
+++ arrays-02-creating-arrays/array.js
@@ -17,20 +17,17 @@ const rowCards = document.querySelector('.row-cards');
 const map = document.querySelector('.map-box');
 
 for (let i = 0; i < +analyticsData[0][1] -1; i++) {
-    console.log(analyticsData[0][1]);
   var e = document.createElement('div');
   e.classList.add('card-box');
   rowCards.appendChild(e);
 }
 
 for (let x = 0; x < +analyticsData[0][2] - 1; x++) {
-    console.log(analyticsData[0][2]);
   var cloneRowCards = rowCards.cloneNode(true);
-  map.appendChild(cloneRowCards);
+    map.appendChild(cloneRowCards);
 } 
 
-console.log(rowCards);
-console.log(map);
+
 /* for (const data of analyticsData) {
   console.log(data);
 } */

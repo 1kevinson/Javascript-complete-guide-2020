607fee94d14e1e89a72d8f60f09b9a4364742deb
Arsene Kevin <kevinkouomeu@icloud.com>
2020-08-26 02:39:15 +0200

    fixed display elements block and inline block

--- arrays-02-creating-arrays/array.js
+++ arrays-02-creating-arrays/array.js
@@ -16,16 +16,18 @@ console.table(analyticsData);
 const rowCards = document.querySelector('.row-cards');
 const map = document.querySelector('.map-box');
 
-for (let index = 0; index < +analyticsData[0][1]; index++) {
+for (let i = 0; i < +analyticsData[0][1] -1; i++) {
+    console.log(analyticsData[0][1]);
   var e = document.createElement('div');
   e.classList.add('card-box');
   rowCards.appendChild(e);
 }
 
 for (let x = 0; x < +analyticsData[0][2] - 1; x++) {
+    console.log(analyticsData[0][2]);
   var cloneRowCards = rowCards.cloneNode(true);
   map.appendChild(cloneRowCards);
-}
+} 
 
 console.log(rowCards);
 console.log(map);
